3e987479ee48d6875f5064d16680f27ceaf8cab8
feifrank <feifrank@yahoo.com>
2016-07-27 08:00:35 -0700

    added button controls.

---
+++ app/src/main/java/com/bignerdranch/android/geoquiz/Toast.java
@@ -0,0 +1,20 @@
+package com.bignerdranch.android.geoquiz;
+
+import android.content.Context;
+
+/**
+ * Created by Frank on 7/27/2016.
+ */
+public class Toast {
+    public static int LENGTH_SHORT = 10;
+    //public int LENGTH_LONG = 20;
+
+    public static Toast makeText(Context context, int resId, int duration) {
+
+        return null;
+    }
+
+    public static void show() {
+
+    }
+}
